5b401510decacecc7a7c0e7d9fc7cb022057c5e3
Ben Shekhtman <benshekhtman@hotmail.com>
2022-05-05 17:37:46 -0400

    styled search and able to follow/unfollow from search

--- components/NavFooter.js
+++ components/NavFooter.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link'
 import navStyles from '../styles/nav-footer.module.scss';
 import { AiOutlineHome, AiFillHome, AiOutlineShopping, AiOutlineSearch } from 'react-icons/ai';
 import { BiMoviePlay } from 'react-icons/bi';
 import { useRouter } from 'next/router'
 import { getCurrentUser } from '../hooks';
+import Search from './search/Search';
 
 const NavFooter = () => {
   const router = useRouter()
   const HOME_ROUTE = '/';
-  const [state] = getCurrentUser();
+  const [state, setState] = getCurrentUser();
+
 
   const currentPath = router.asPath;
   const currrentUserProfilePath = state.currentUser?.id ? `/profile` : null;
@@ -20,24 +22,35 @@ const NavFooter = () => {
     })
   }
 
-  return (
-    <nav className={navStyles.nav}>
-      <ul>
-        <Link href={HOME_ROUTE}>
-          <li>{currentPath === HOME_ROUTE ? (<AiFillHome />) : (<AiOutlineHome />)}</li>
-        </Link>
+  const [showSearch, setShowSearch] = useState(false);
+  const [currentTopPosition, setCurrentTopPositition] = useState(null)
 
-        <li><AiOutlineSearch /></li>
-        <li><BiMoviePlay /></li>
-        <li><AiOutlineShopping /></li>
-        <li onClick={handleViewProfile}>
-          <img
-            style={currrentUserProfilePath === currentPath ? { border: '2px solid white' } : null}
-            src={state.currentUser && state.currentUser.avatar ? state.currentUser.avatar : 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E'}
-            alt='user profile img' />
-        </li>
-      </ul>
-    </nav>
+  const handleSearch = () => {
+    setShowSearch(true);
+    setCurrentTopPositition(window.pageYOffset)
+  }
+
+  return (
+    <React.Fragment>
+      <nav className={navStyles.nav}>
+        <ul>
+          <Link href={HOME_ROUTE}>
+            <li>{currentPath === HOME_ROUTE ? (<AiFillHome />) : (<AiOutlineHome />)}</li>
+          </Link>
+
+          <li><AiOutlineSearch onClick={handleSearch} /></li>
+          <li><BiMoviePlay /></li>
+          <li><AiOutlineShopping /></li>
+          <li onClick={handleViewProfile}>
+            <img
+              style={currrentUserProfilePath === currentPath ? { border: '2px solid white' } : null}
+              src={state.currentUser && state.currentUser.avatar ? state.currentUser.avatar : 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E'}
+              alt='user profile img' />
+          </li>
+        </ul>
+      </nav>
+      <Search state={state ? state : null} setState={setState} showSearch={showSearch} setShowSearch={setShowSearch} currentTopPosition={currentTopPosition} />
+    </React.Fragment>
   )
 }
 
--- components/search/Search.js
+++ components/search/Search.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { SEARCH_USERS } from '../../utils/mutations';
+import { FOLLOW_USER, UNFOLLOW_USER } from '../../utils/mutations';
 import { useMutation } from '@apollo/client';
+import searchStyles from '../../styles/search.module.scss';
+import Overlay from '../Overlay';
+import { MdOutlineArrowBackIosNew } from 'react-icons/md';
+import { AiOutlineConsoleSql, AiOutlineSearch } from 'react-icons/ai';
 
-const Search = () => {
+
+const Search = ({ state, showSearch, setShowSearch, currentTopPosition, setState }) => {
   const [inputVal, setInputVal] = useState('');
   const [searchUsers] = useMutation(SEARCH_USERS);
   const [results, setResults] = useState([]);
@@ -39,22 +45,104 @@ const Search = () => {
 
   useEffect(() => {
     if (results.length === 0 && inputVal !== '') {
-      setNoResultsMsg('No Users Found : (');
+      setNoResultsMsg('No results.');
     } else if (results.length > 0 && inputVal !== '') {
       setNoResultsMsg('');
     }
   }, [results, inputVal])
 
+
+  const IsResultFollowingUser = ({ user }) => {
+    const [followUser] = useMutation(FOLLOW_USER);
+    const [unfollowUser] = useMutation(UNFOLLOW_USER);
+    const [isFollowing, setIsFollowing] = useState(true);
+
+    const handlefollow = async () => {
+      try {
+        const { data } = await followUser({
+          variables: {
+            followedByUserId: Number(state.currentUser.id),
+            followingUserId: Number(user.id)
+          }
+        });
+        if (data) {
+          setState({
+            ...state,
+            currentUser: {
+              ...state.currentUser,
+              following: [user, ...state.currentUser.following]
+            }
+          })
+          setIsFollowing(true);
+        }
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+    const handleUnfollow = async () => {
+      try {
+        const { data } = await unfollowUser({
+          variables: {
+            userId: Number(state.currentUser.id),
+            userIdToUnfollow: Number(user.id)
+          }
+        });
+        if (data) {
+          setState({
+            ...state,
+            currentUser: {
+              ...state.currentUser,
+              following: state.currentUser.following.filter((follow) => follow.id !== user.id)
+            }
+          })
+          setIsFollowing(false);
+        }
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+
+    useEffect(() => {
+      const ids = state.currentUser.following.map((follow) => follow.id)
+      setIsFollowing(ids.includes(user.id))
+    }, [])
+
+    return (
+      isFollowing ? (
+        <button onClick={handleUnfollow} className={searchStyles.btnFollowing}>Following</button>
+      ) : (
+        <button onClick={handlefollow} className={searchStyles.btnFollow}>Follow</button>
+      )
+    )
+  }
+
   return (
-    <div>
-      <input value={inputVal} onChange={handleSearch} />
-      {results.map((user, idx) => (
-        <div key={user.id}>
-          <p> {idx + 1}: {user.userName}</p>
-        </div>
-      ))}
-      <p>{noResultsMsg}</p>
-    </div>
+    <Overlay currentTopPosition={currentTopPosition} height={'100vh'} isShowing={showSearch}>
+      <nav className={searchStyles.nav}>
+        <MdOutlineArrowBackIosNew onClick={() => setShowSearch(false)} />
+        <h2>Search Users</h2>
+      </nav>
+      <div className={searchStyles.input}>
+        <AiOutlineSearch />
+        <input placeholder='Search for users' value={inputVal} onChange={handleSearch} />
+      </div>
+
+      <div className={searchStyles.results}>
+        {results.map((user) => (
+          <div key={user.id}>
+            <img src={user.avatar} />
+            <div className={searchStyles.details}>
+              <h3>{user.userName}</h3>
+              <p>{user.name}</p>
+            </div>
+            {state.currentUser.id === user.id ? null : (
+              <IsResultFollowingUser user={user} />
+            )}
+          </div>
+        ))}
+        <p className={searchStyles.noResults}>{noResultsMsg}</p>
+      </div>
+    </Overlay>
   )
 }
 
